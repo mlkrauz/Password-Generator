82d6bba299dac865d1274a0d6aca7bf04b2aa3a0
ElusiveEntity <30778577+ElusiveEntity@users.noreply.github.com>
2021-06-13 15:57:56 -0400

    Fix remaining bugs within cancel functions (that I'm aware). Began function for character type selection.

--- assets/js/script.js
+++ assets/js/script.js
@@ -15,40 +15,54 @@ generateBtn.addEventListener("click", writePassword);
 
 
 
-//Main function to generate password
+//------------------------------------
+//-----------Main Function------------
+//------------------------------------
 function generatePassword() {
   //By putting the content of the password generation within a try..catch, we can exit if the user cancels.
   try {
     var passLength = promptLength();
-    var characterTypes = [];
+    var characterTypes = promptCharacterTypes();
 
     return "" + passLength;
 
   } catch (error) {
-    //This will be returned if the labeled block above is broken by canceling. Or if something breaks.
+    //This will be returned if the labeled block above is broken by canceling. Or if something breaks. Your guess is mine.
     return "Password generation cancelled."
   }
 }
 
+//------------------------------------
+//----------Prompt Functions----------
+//------------------------------------
 function promptLength() {
   var userInput = handlePrompt("What is the desired character length of your password? (8-128): ", 32);
 
-  if (isNaN(Number(userInput)) || userInput <= 8 || userInput >= 128) {
+  if (!isNaN(Number(userInput)) && userInput >= 8 && userInput <= 128) {
     return userInput;
+  } else if (userInput == null) { //This occurs when a prompt is cancelled, but the user returns to resume.
+     return promptLength();  
   } else {
     alert("Invalid selection. Please try again!");
     return promptLength();
   }
 }
 
+function promptCharacterTypes() {
+  var characterTypes = [lowerCase, upperCase, numerics, specialChars];
+}
+
 //------------------------------------
 //----------Helper Functions----------
 //------------------------------------
-
 function handlePrompt(message, defaultAnswer) {
   var userInput = prompt(message, defaultAnswer);
-  checkForCancel(userInput);
-  return userInput;
+  if (checkForCancel(userInput)) {
+    return userInput;
+  } else {
+    //User cancelled but then returned.
+    return null;
+  }
 }
 
 function checkForCancel(userInput) {
@@ -57,6 +71,12 @@ function checkForCancel(userInput) {
     if (cancelConfirm == true) {
       //Throw an error to cancel
       throw "User cancellation exception."
-    }
+    } else {
+      //Return false so we understand when the "cancelled cancel" situation occurs.
+      return false;
+    }  
+  } else {
+    //input is not null, user did not cancel.
+    return true;
   }
 }
