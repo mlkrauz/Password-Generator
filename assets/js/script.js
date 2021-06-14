362d509d188a5e50df575489d0e5a8155ebd0732
ElusiveEntity <30778577+ElusiveEntity@users.noreply.github.com>
2021-06-13 20:57:37 -0400

    Completed functionality for the character selector.

--- assets/js/script.js
+++ assets/js/script.js
@@ -22,9 +22,9 @@ function generatePassword() {
   //By putting the content of the password generation within a try..catch, we can exit if the user cancels.
   try {
     var passLength = promptLength();
-    var characterTypes = promptCharacterTypes();
+    var characterList = promptCharacterTypes();
 
-    return "" + passLength;
+    return passLength, characterList;
 
   } catch (error) {
     //This will be returned if the labeled block above is broken by canceling. Or if something breaks. Your guess is mine.
@@ -43,13 +43,49 @@ function promptLength() {
   } else if (userInput == null) { //This occurs when a prompt is cancelled, but the user returns to resume.
      return promptLength();  
   } else {
-    alert("Invalid selection. Please try again!");
+      alert("Invalid selection. Please try again!");
     return promptLength();
   }
 }
 
 function promptCharacterTypes() {
-  var characterTypes = [lowerCase, upperCase, numerics, specialChars];
+  var userInput = handlePrompt("What combination of character types should your password contain? " + 
+    "Type 1 for lower case, 2 for upper case, 3 for numbers, 4 for special characters.", "1234")
+
+  if (userInput.includes("1") || userInput.includes("2") || userInput.includes("3") || userInput.includes("4")) {
+    var characterList = [];
+
+    //Update characterList to reflect the selected types.
+    for (var i = 1; i <= 4; i++) {
+      if (userInput.includes(i)) {
+        switch (i) {
+          case 1:
+            characterList = characterList.concat(['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']);
+            break;
+          case 2:
+            characterList = characterList.concat(['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']);
+            break;
+          case 3:
+            characterList = characterList.concat(['0','1','2','3','4','5','6','7','8','9']);
+            break;
+          case 4:
+            characterList = characterList.concat(['!','\"','#','$','%','&','\'','(',')','*','+',',','-','.','/',':',';',
+              '<','=','>','?','@','[','\\',']','^','_','`','{','|','}','~']);
+            break;
+        }
+      }
+    }
+    //And return that list
+    console.log(characterList);
+    return characterList;
+
+  } else if (userInput == null) {
+      return promptCharacterTypes();
+  } else {
+      alert("Invalid selection. Please try again!");
+      return promptCharacterTypes();
+  }
+
 }
 
 //------------------------------------
